d4aab705dc150f6507b7ad6f384a9f3ac974b2d4
margohanyz <malgorzata.hanyz@student.put.poznan.pl>
2020-01-11 22:37:02 +0100

    init

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 import functions as f
-import os, nltk
+import os, nltk, re
 #nltk.download('punkt')
 #nltk.download('averaged_perceptron_tagger')
 #nltk.download('universal_tagset')
 #nltk.download('maxent_ne_chunker')
 #nltk.download('words')
+#nltk.download('ace')
+
+NE_CLASSES = {
+    'ieer': [
+        'LOCATION',
+        'ORGANIZATION',
+        'PERSON',
+        'DURATION',
+        'DATE',
+        'CARDINAL',
+        'PERCENT',
+        'MONEY',
+        'MEASURE',
+    ],
+    'conll2002': ['LOC', 'PER', 'ORG'],
+    'ace': [
+        'LOCATION',
+        'ORGANIZATION',
+        'PERSON',
+        'DURATION',
+        'DATE',
+        'CARDINAL',
+        'PERCENT',
+        'MONEY',
+        'MEASURE',
+        'FACILITY',
+        'GPE',
+    ],
+}
 
 sentence_list = []
 basepath = 'training/'
@@ -17,6 +46,7 @@ tokens = f.ie_preprocess(sentence_list)
 
 #grammar = "NP: {<DET|PRON\$>?<ADJ>*<NOUN>+}"
 grammar2 = r"""
+  PERSON: {<PERSON><PERSON>+}
   NP: {<DT|JJ|NN.*>+}          # Chunk sequences of DT, JJ, NN
   PP: {<IN><NP>}               # Chunk prepositions followed by NP
   VP: {<VB.*><NP|PP|CLAUSE>+$} # Chunk verbs and their arguments
@@ -26,9 +56,21 @@ cp = nltk.RegexpParser(grammar2)
 
 chunks = []
 for t in tokens:
-    chunk = cp.parse(t)
-    chunk2 = nltk.ne_chunk(chunk)
+    chunk = nltk.ne_chunk(t)
+    chunk2 = cp.parse(chunk)
     chunks.append(chunk2)
 
+#print(type(chunks[4]))
+
+IN = re.compile(r'.*\bin\b(?!\b.+ing)')
+IN2 = re.compile(r'.*\bin\b.*')
+X = re.compile(r'\b')
+
+#for doc in nltk.corpus.ieer.parsed_docs('NYT_19980315'):
+#    for rel in nltk.sem.extract_rels('ORG', 'LOC', doc, corpus='ieer', pattern = IN):
+#        print(nltk.sem.rtuple(rel))
+
+for rel in nltk.sem.extract_rels('PER', 'ORG', chunks, pattern = IN):
+    print(nltk.sem.rtuple(rel))
 
 x = 1
